33a770f35a74cb4a1d1a7de72744902448e26356
ErickZaltman <amanesko@gmail.com>
2017-11-21 16:27:05 +0300

    adsfas

--- Contract/cs/com.cs
+++ Contract/cs/com.cs
@@ -12,14 +12,10 @@ namespace Contract
 
         public string Value { get; set; }
 
-        public static TableName InnerMail { get { return new TableName("InnerMail"); } }
-        public static TableName Departments { get { return new TableName("Departments"); } }
+        public static TableName Category { get { return new TableName("Category"); } }
+        public static TableName Users { get { return new TableName("Users"); } }
         public static TableName Contractors { get { return new TableName("Contractors"); } }
-        public static TableName IncomingMail { get { return new TableName("IncomingMail"); } }
-        public static TableName OutgoingMail { get { return new TableName("OutgoingMail"); } }
-        public static TableName ServiceMail { get { return new TableName("ServiceMail"); } }
-        public static TableName TreatmentMail { get { return new TableName("TreatmentMail"); } }
-        public static TableName UserInfo { get { return new TableName("UserInfo"); } }
-        public static TableName User { get { return new TableName("User"); } }
+        public static TableName Departments { get { return new TableName("Departments"); } }
+
     }
 }

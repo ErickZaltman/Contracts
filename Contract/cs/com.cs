5ef1c91beafb53eff8e15332fb1ae188e0f17c7d
Zabivalkin <dmitriy.zabi@gmail..com>
2017-11-22 09:14:49 +0300

    dlmkcvhdbf

---
+++ Contract/cs/com.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract
+{
+    public class TableName
+    {
+        private TableName(string value) { Value = value; }
+
+        public string Value { get; set; }
+
+        public static TableName Category { get { return new TableName("Category"); } }
+        public static TableName Users { get { return new TableName("Users"); } }
+        public static TableName Contractors { get { return new TableName("Contractors"); } }
+        public static TableName Departments { get { return new TableName("Departments"); } }
+
+    }
+}

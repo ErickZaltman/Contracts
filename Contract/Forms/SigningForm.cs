6858596b1f1bde9ee801076a8d56eaf553f3a30f
ErickZaltman <amanesko@gmail.com>
2017-12-12 16:40:25 +0300

    aaaa

--- Contract/Forms/SigningForm.cs
+++ Contract/Forms/SigningForm.cs
@@ -13,7 +13,7 @@ namespace Contract.Forms
 {
     public partial class SigningForm : DevExpress.XtraEditors.XtraForm
     {
-        //private UpdateContracts d;
+        //private UpdateContracts d;aaa
         private DB.DBModel dbContext;
         private int contractID;
         private int signID;
